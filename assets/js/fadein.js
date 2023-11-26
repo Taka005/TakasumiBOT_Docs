dba15f1ec97d5541cf25fdc6b4111ec7d49af9a0
Taka005 <01taka005@gmail.com>
2023-11-26 17:35:17 +0900

    test

--- assets/js/fadein.js
+++ assets/js/fadein.js
@@ -2,6 +2,8 @@ document.addEventListener("scroll",()=>{
   document.querySelectorAll(".fadein").forEach(ele=>{
     const pos = ele.offsetTop - window.innerHeight;
 
+    console.log(pos)
+    console.log(window.scrollY)
     if(window.scrollY > pos + 200){
       ele.classList.add("scrollin");
     }
