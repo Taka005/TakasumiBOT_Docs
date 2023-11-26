4a8a38a20d8d213b34dcb025d41c077a84332954
Taka005 <01taka005@gmail.com>
2023-11-26 18:22:13 +0900

    fix

--- assets/js/fadein.js
+++ assets/js/fadein.js
@@ -2,9 +2,7 @@ document.addEventListener("scroll",()=>{
   document.querySelectorAll(".fadein").forEach(ele=>{
     const pos = ele.offsetTop - window.innerHeight;
 
-    console.log(pos)
-    console.log(window.scrollY)
-    if(-window.scrollY > pos + 200){
+    if(window.scrollY > pos + 200){
       ele.classList.add("scrollin");
     }
   });
